ff1e6219d9adbda24dbb49047f3993358e6c1a3b
Rodolfo Dias Venancio <darth.rodolfo@gmail.com>
2021-02-02 21:12:23 -0300

    Altering dashboard screen code

--- lib/screens/dashboard/dashboard.dart
+++ lib/screens/dashboard/dashboard.dart
@@ -3,16 +3,11 @@ import 'package:flutter/material.dart';
 class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        accentColor: Colors.blueAccent[700],
-        primaryColor: Colors.green[900],
-        buttonTheme: ButtonThemeData(
-          buttonColor: Colors.blueAccent[700],
-          textTheme: ButtonTextTheme.primary,
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bytebank'),
       ),
-      home: Dashboard(),
+      body: Text('40.00'),
     );
   }
 }
